7e87ceab732a1cf75808d5d9b7e71332b7e02574
nabihakhan6t4 <khannabiha923@gmail.com>
2024-08-18 17:51:40 +0300

    object design

---
+++ script.js
@@ -0,0 +1,538 @@
+var makeupProducts = {
+  Maybelline: {
+    NewYork: {
+      img: "images/lipstick.jpg",
+      productName: "Lipstick",
+      shade: "Red",
+      price: "$10",
+    },
+    London: {
+      img: "foundation-beige.jfif",
+      productName: "Foundation",
+      shade: "Beige",
+      price: "$15",
+    },
+    Paris: {
+      img: "mascara-black.jfif",
+      productName: "Mascara",
+      shade: "Black",
+      price: "$12",
+    },
+    Tokyo: {
+      img: "eyeshadow-gold.jfif",
+      productName: "Eyeshadow",
+      shade: "Gold",
+      price: "$20",
+    },
+    Sydney: {
+      img: "blush-pink.jfif",
+      productName: "Blush",
+      shade: "Pink",
+      price: "$18",
+    },
+  },
+  LOreal: {
+    Paris: {
+      img: "lipstick-nude.jfif",
+      productName: "Lipstick",
+      shade: "Nude",
+      price: "$12",
+    },
+    NewYork: {
+      img: "foundation-medium.jfif",
+      productName: "Foundation",
+      shade: "Medium",
+      price: "$18",
+    },
+    Tokyo: {
+      img: "mascara-brown.jfif",
+      productName: "Mascara",
+      shade: "Brown",
+      price: "$14",
+    },
+    Sydney: {
+      img: "eyeshadow-silver.jfif",
+      productName: "Eyeshadow",
+      shade: "Silver",
+      price: "$22",
+    },
+    London: {
+      img: "blush-peach.jfif",
+      productName: "Blush",
+      shade: "Peach",
+      price: "$16",
+    },
+  },
+  Revlon: {
+    NewYork: {
+      img: "lipgloss-clear.jfif",
+      productName: "Lipgloss",
+      shade: "Clear",
+      price: "$8",
+    },
+    Paris: {
+      img: "foundation-dark.jfif",
+      productName: "Foundation",
+      shade: "Dark",
+      price: "$20",
+    },
+    Tokyo: {
+      img: "mascara-volumizing.jfif",
+      productName: "Mascara",
+      shade: "Volumizing",
+      price: "$17",
+    },
+    London: {
+      img: "eyeshadow-bronze.jfif",
+      productName: "Eyeshadow",
+      shade: "Bronze",
+      price: "$19",
+    },
+    Sydney: {
+      img: "blush-coral.jfif",
+      productName: "Blush",
+      shade: "Coral",
+      price: "$15",
+    },
+  },
+  MAC: {
+    Toronto: {
+      img: "lipstick-matte.jfif",
+      productName: "Lipstick",
+      shade: "Matte",
+      price: "$25",
+    },
+    Paris: {
+      img: "foundation-light.jfif",
+      productName: "Foundation",
+      shade: "Light",
+      price: "$30",
+    },
+    NewYork: {
+      img: "mascara-waterproof.jfif",
+      productName: "Mascara",
+      shade: "Waterproof",
+      price: "$22",
+    },
+    Tokyo: {
+      img: "eyeshadow-plum.jfif",
+      productName: "Eyeshadow",
+      shade: "Plum",
+      price: "$28",
+    },
+    Sydney: {
+      img: "blush-rose.jfif",
+      productName: "Blush",
+      shade: "Rose",
+      price: "$23",
+    },
+  },
+  NARS: {
+    Tokyo: {
+      img: "lipstick-mauve.jfif",
+      productName: "Lipstick",
+      shade: "Mauve",
+      price: "$24",
+    },
+    Paris: {
+      img: "foundation-medium-dark.jfif",
+      productName: "Foundation",
+      shade: "Medium Dark",
+      price: "$27",
+    },
+    London: {
+      img: "mascara-lengthening.jfif",
+      productName: "Mascara",
+      shade: "Lengthening",
+      price: "$19",
+    },
+    NewYork: {
+      img: "eyeshadow-copper.jfif",
+      productName: "Eyeshadow",
+      shade: "Copper",
+      price: "$26",
+    },
+    Sydney: {
+      img: "blush-berry.jfif",
+      productName: "Blush",
+      shade: "Berry",
+      price: "$22",
+    },
+  },
+  UrbanDecay: {
+    NewYork: {
+      img: "lipstick-berry.jfif",
+      productName: "Lipstick",
+      shade: "Berry",
+      price: "$21",
+    },
+    Paris: {
+      img: "foundation-full-coverage.jfif",
+      productName: "Foundation",
+      shade: "Full Coverage",
+      price: "$29",
+    },
+    Tokyo: {
+      img: "mascara-volumizing.jfif",
+      productName: "Mascara",
+      shade: "Volumizing",
+      price: "$23",
+    },
+    London: {
+      img: "eyeshadow-emerald.jfif",
+      productName: "Eyeshadow",
+      shade: "Emerald",
+      price: "$27",
+    },
+    Sydney: {
+      img: "blush-mauve.jfif",
+      productName: "Blush",
+      shade: "Mauve",
+      price: "$20",
+    },
+  },
+  Benefit: {
+    NewYork: {
+      img: "lipstick-pink.jfif",
+      productName: "Lipstick",
+      shade: "Pink",
+      price: "$18",
+    },
+    Paris: {
+      img: "foundation-ivory.jfif",
+      productName: "Foundation",
+      shade: "Ivory",
+      price: "$22",
+    },
+    Tokyo: {
+      img: "mascara-dark-brown.jfif",
+      productName: "Mascara",
+      shade: "Dark Brown",
+      price: "$16",
+    },
+    London: {
+      img: "eyeshadow-gold.jfif",
+      productName: "Eyeshadow",
+      shade: "Gold",
+      price: "$24",
+    },
+    Sydney: {
+      img: "blush-bubblegum.jfif",
+      productName: "Blush",
+      shade: "Bubblegum",
+      price: "$18",
+    },
+  },
+  TooFaced: {
+    NewYork: {
+      img: "lipstick-caramel.jfif",
+      productName: "Lipstick",
+      shade: "Caramel",
+      price: "$19",
+    },
+    Paris: {
+      img: "foundation-tan.jfif",
+      productName: "Foundation",
+      shade: "Tan",
+      price: "$25",
+    },
+    Tokyo: {
+      img: "mascara-blackest-black.jfif",
+      productName: "Mascara",
+      shade: "Blackest Black",
+      price: "$21",
+    },
+    London: {
+      img: "eyeshadow-platinum.jfif",
+      productName: "Eyeshadow",
+      shade: "Platinum",
+      price: "$28",
+    },
+    Sydney: {
+      img: "blush-papaya.jfif",
+      productName: "Blush",
+      shade: "Papaya",
+      price: "$22",
+    },
+  },
+  Tarte: {
+    NewYork: {
+      img: "lipstick-mahogany.jfif",
+      productName: "Lipstick",
+      shade: "Mahogany",
+      price: "$22",
+    },
+    Paris: {
+      img: "foundation-deep.jfif",
+      productName: "Foundation",
+      shade: "Deep",
+      price: "$28",
+    },
+    Tokyo: {
+      img: "mascara-jet-black.jfif",
+      productName: "Mascara",
+      shade: "Jet Black",
+      price: "$20",
+    },
+    London: {
+      img: "eyeshadow-rose-gold.jfif",
+      productName: "Eyeshadow",
+      shade: "Rose Gold",
+      price: "$26",
+    },
+    Sydney: {
+      img: "blush-terracotta.jfif",
+      productName: "Blush",
+      shade: "Terracotta",
+      price: "$24",
+    },
+  },
+  Smashbox: {
+    NewYork: {
+      img: "lipstick-crimson.jfif",
+      productName: "Lipstick",
+      shade: "Crimson",
+      price: "$20",
+    },
+    Paris: {
+      img: "foundation-neutral.jfif",
+      productName: "Foundation",
+      shade: "Neutral",
+      price: "$24",
+    },
+    Tokyo: {
+      img: "mascara-volumizer.jfif",
+      productName: "Mascara",
+      shade: "Volumizer",
+      price: "$22",
+    },
+    London: {
+      img: "eyeshadow-rose.jfif",
+      productName: "Eyeshadow",
+      shade: "Rose",
+      price: "$25",
+    },
+    Sydney: {
+      img: "blush-sunset.jfif",
+      productName: "Blush",
+      shade: "Sunset",
+      price: "$21",
+    },
+  },
+  Hourglass: {
+    NewYork: {
+      img: "lipstick-rosewood.jfif",
+      productName: "Lipstick",
+      shade: "Rosewood",
+      price: "$26",
+    },
+    Paris: {
+      img: "foundation-powder.jfif",
+      productName: "Foundation",
+      shade: "Powder",
+      price: "$30",
+    },
+    Tokyo: {
+      img: "mascara-bold.jfif",
+      productName: "Mascara",
+      shade: "Bold",
+      price: "$24",
+    },
+    London: {
+      img: "eyeshadow-ivory.jfif",
+      productName: "Eyeshadow",
+      shade: "Ivory",
+      price: "$28",
+    },
+    Sydney: {
+      img: "blush-dusty-rose.jfif",
+      productName: "Blush",
+      shade: "Dusty Rose",
+      price: "$25",
+    },
+  },
+  FentyBeauty: {
+    NewYork: {
+      img: "lipstick-uncensored.jfif",
+      productName: "Lipstick",
+      shade: "Uncensored",
+      price: "$28",
+    },
+    Paris: {
+      img: "foundation-150.jfif",
+      productName: "Foundation",
+      shade: "150",
+      price: "$32",
+    },
+    Tokyo: {
+      img: "mascara-lengthening.jfif",
+      productName: "Mascara",
+      shade: "Lengthening",
+      price: "$26",
+    },
+    London: {
+      img: "eyeshadow-caramel.jfif",
+      productName: "Eyeshadow",
+      shade: "Caramel",
+      price: "$30",
+    },
+    Sydney: {
+      img: "blush-ginger.jfif",
+      productName: "Blush",
+      shade: "Ginger",
+      price: "$27",
+    },
+  },
+  LauraMercier: {
+    NewYork: {
+      img: "lipstick-sable.jfif",
+      productName: "Lipstick",
+      shade: "Sable",
+      price: "$23",
+    },
+    Paris: {
+      img: "foundation-honey.jfif",
+      productName: "Foundation",
+      shade: "Honey",
+      price: "$25",
+    },
+    Tokyo: {
+      img: "mascara-darkness.jfif",
+      productName: "Mascara",
+      shade: "Darkness",
+      price: "$22",
+    },
+    London: {
+      img: "eyeshadow-mocha.jfif",
+      productName: "Eyeshadow",
+      shade: "Mocha",
+      price: "$26",
+    },
+    Sydney: {
+      img: "blush-rosebud.jfif",
+      productName: "Blush",
+      shade: "Rosebud",
+      price: "$22",
+    },
+  },
+  NYX: {
+    NewYork: {
+      img: "lipstick-teddy.jfif",
+      productName: "Lipstick",
+      shade: "Teddy",
+      price: "$10",
+    },
+    Paris: {
+      img: "foundation-warm.jfif",
+      productName: "Foundation",
+      shade: "Warm",
+      price: "$14",
+    },
+    Tokyo: {
+      img: "mascara-length.jfif",
+      productName: "Mascara",
+      shade: "Length",
+      price: "$12",
+    },
+    London: {
+      img: "eyeshadow-plum.jfif",
+      productName: "Eyeshadow",
+      shade: "Plum",
+      price: "$15",
+    },
+    Sydney: {
+      img: "blush-peachy.jfif",
+      productName: "Blush",
+      shade: "Peachy",
+      price: "$11",
+    },
+  },
+  KatVonD: {
+    NewYork: {
+      img: "lipstick-vampire.jfif",
+      productName: "Lipstick",
+      shade: "Vampire",
+      price: "$24",
+    },
+    Paris: {
+      img: "foundation-fair.jfif",
+      productName: "Foundation",
+      shade: "Fair",
+      price: "$27",
+    },
+    Tokyo: {
+      img: "mascara-intense.jfif",
+      productName: "Mascara",
+      shade: "Intense",
+      price: "$22",
+    },
+    London: {
+      img: "eyeshadow-sapphire.jfif",
+      productName: "Eyeshadow",
+      shade: "Sapphire",
+      price: "$28",
+    },
+    Sydney: {
+      img: "blush-rosewood.jfif",
+      productName: "Blush",
+      shade: "Rosewood",
+      price: "$24",
+    },
+  },
+  BiteBeauty: {
+    NewYork: {
+      img: "lipstick-cherry.jfif",
+      productName: "Lipstick",
+      shade: "Cherry",
+      price: "$26",
+    },
+    Paris: {
+      img: "foundation-neutral.jfif",
+      productName: "Foundation",
+      shade: "Neutral",
+      price: "$30",
+    },
+    Tokyo: {
+      img: "mascara-black-jack.jfif",
+      productName: "Mascara",
+      shade: "Black Jack",
+      price: "$25",
+    },
+    London: {
+      img: "eyeshadow-fuchsia.jfif",
+      productName: "Eyeshadow",
+      shade: "Fuchsia",
+      price: "$27",
+    },
+    Sydney: {
+      img: "blush-bloom.jfif",
+      productName: "Blush",
+      shade: "Bloom",
+      price: "$22",
+    },
+  },
+};
+
+var brand = document.querySelector(".brand");
+var country = document.querySelector(".country");
+var cards = document.querySelector(".cards");
+
+brand.innerHTML = ` <option selected>Select Brand</option>`;
+country.innerHTML = ` <option selected>Select Country</option>`;
+for (var key in makeupProducts) {
+  console.log(key);
+  cards.innerHTML = `<div class="card" style="width: 18rem;">
+  <img src="images/blush.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`;
+  brand.innerHTML += `<option value="${key}">${key}</option>`;
+}
+function setCountry() {
+  for (var key1 in makeupProducts[key]) {
+    country.innerHTML += `<option value="${key1}">${key1}</option>`;
+  }
+}
